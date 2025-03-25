1b3ef859cb0aa3c67fb36b84c837118904ab1a7b
HarshadSanjayKarale <harshadkarale0@gmail.com>
2025-03-25 09:47:16 +0530

    deploy

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,4 @@
-workers = 2  # Reduce from 4 to 2
+workers = 4
 threads = 2
-timeout = 60
+timeout = 60  # Increase timeout to 60 seconds
 bind = "0.0.0.0:5000"
-max_requests = 500
-max_requests_jitter = 100
-worker_class = 'gthread'
-preload_app = False  # Don't preload the app to save memory
